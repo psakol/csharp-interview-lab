f2a6ff22d356cd1121876e4ca41164c334f22f12
PARTHASARATHI ADHIKARY <parthasarathi.adhikary@gmail.com>
2026-07-29 23:30:18 +0530

    Add Employee class with properties

---
+++ src/01-LINQ/Employee.cs
@@ -0,0 +1,10 @@
+namespace LinqExamples;
+
+public class Employee
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string Department { get; init; } = string.Empty;
+    public decimal Salary { get; init; }
+    public int Age { get; init; }
+}
